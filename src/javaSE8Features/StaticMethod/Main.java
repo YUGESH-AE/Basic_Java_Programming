fb5e2e69d9ce769a6945204f0b5dbad35e839ae4
YUGESH-AE <yugeshae@gmail.com>
2022-05-19 11:22:02 +0530

    Initial commit

---
+++ src/javaSE8Features/StaticMethod/Main.java
@@ -0,0 +1,18 @@
+package javaSE8Features.StaticMethod;
+
+interface Myinterface{
+    static void display(){
+        System.out.println("Displaying in interface");
+    }
+}
+class A implements Myinterface{
+    public void show(){
+        System.out.println("showing");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Myinterface.display();
+    }
+}
