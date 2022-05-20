6821e41e9a6559c106932915ee2dcfeacc5e9999
YUGESH-AE <yugeshae@gmail.com>
2022-05-20 16:15:48 +0530

    Date/Time API

---
+++ src/javaSE8Features/Date_Time_API/LocalDate/RemainderMail/Remainder.java
@@ -0,0 +1,13 @@
+package javaSE8Features.Date_Time_API.LocalDate.RemainderMail;
+
+import java.time.LocalDate;
+
+public class Remainder {
+    public static void main(String[] args) {
+        LocalDate today=LocalDate.now();
+        LocalDate future=LocalDate.now().plusDays(3);
+        if(today.isAfter(future)){
+            System.out.println("Time to write the course");
+        }
+    }
+}
