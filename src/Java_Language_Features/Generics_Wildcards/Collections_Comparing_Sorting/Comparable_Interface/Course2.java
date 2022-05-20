d288592bea807b66dfea7f4681ccfe3b41ad5d75
YUGESH-AE <yugeshae@gmail.com>
2022-05-20 18:40:09 +0530

    Comparator,comparable

---
+++ src/Java_Language_Features/Generics_Wildcards/Collections_Comparing_Sorting/Comparable_Interface/Course2.java
@@ -0,0 +1,40 @@
+package Java_Language_Features.Generics_Wildcards.Collections_Comparing_Sorting.Comparable_Interface;
+
+public class Course2 implements Comparable<Course2> {
+    private int courseId;
+    private String courseName;
+
+    public Course2(int courseId, String courseName) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    @Override
+    public String toString() {
+        return "Course1{" +
+                "courseId=" + courseId +
+                ", courseName='" + courseName + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Course2 other) {
+        return courseName.compareTo(other.courseName);
+    }
+}
