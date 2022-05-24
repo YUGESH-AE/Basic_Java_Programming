7034b5d3e34c9f55618e2f9e4c88a85f188e5ca0
YUGESH-AE <yugeshae@gmail.com>
2022-05-24 12:41:41 +0530

    Optional class

---
+++ src/Java_Language_Features/Generics_Wildcards/Concurrency/Executor_Framework/MainDEmoexe.java
@@ -0,0 +1,20 @@
+package Java_Language_Features.Generics_Wildcards.Concurrency.Executor_Framework;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainDEmoexe {
+    public static void main(String[] args) {
+        upload a=new upload();
+        ExecutorService exservice= Executors.newSingleThreadExecutor();
+        exservice.execute(a);
+
+    }
+}
+class upload extends Thread{
+    @Override
+    public void run() {
+        System.out.println("Executing");
+    }
+}
