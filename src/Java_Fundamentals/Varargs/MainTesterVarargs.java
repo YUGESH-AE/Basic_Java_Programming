241e4350d1f85cfb26d63bfa95664a394099f77c
YUGESH-AE <yugeshae@gmail.com>
2022-05-25 17:09:02 +0530

    Enum,Varargs

---
+++ src/Java_Fundamentals/Varargs/MainTesterVarargs.java
@@ -0,0 +1,14 @@
+package Java_Fundamentals.Varargs;
+
+public class MainTesterVarargs {
+    public static void main(String[] args) {
+        display(10,20,30,40,50);
+    }
+    public static void display(int...a){
+        for (int x:a){
+            System.out.print(x+"\t");
+        }
+    }
+}
+
+
