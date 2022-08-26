7dd61514a918a6068a363654ae85aef1557dbb27
YUGESH-AE <103350026+YUGESH-AE@users.noreply.github.com>
2022-08-26 15:19:17 +0530

    collection framework

---
+++ src/Java_Language_Features/Generics_Wildcards/CollectionFrameWork/Map/MapHash.java
@@ -0,0 +1,24 @@
+package Java_Language_Features.Generics_Wildcards.CollectionFrameWork.Map;
+
+import java.util.*;
+
+public class MapHash {
+
+
+    public static void main(String args[]) {
+        HashMap<String, Integer> hm = new HashMap<String, Integer>();
+        hm.put("a", 100);
+        hm.put("b", 200);
+        hm.put("c", 300);
+        hm.put("d", 400);
+        hm.put(null, null);
+
+        Set<Map.Entry<String, Integer>> st = hm.entrySet();  //returns Set view
+        for (Map.Entry<String, Integer> me : st) {
+            System.out.print(me.getKey() + ":");
+            System.out.println(me.getValue());
+        }
+    }
+}
+
+
